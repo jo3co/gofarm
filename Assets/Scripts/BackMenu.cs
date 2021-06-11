8866ed36a7646917c22487cfd05f551541adf05e
Henrik Doark <henrikdoark@gmail.com>
2021-06-10 22:19:24 -0300

    Modifed EndGame with a new score animation

--- Assets/Scripts/BackMenu.cs
+++ Assets/Scripts/BackMenu.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class BackMenu : MonoBehaviour
 {
-    public Text scoreText;
+    //public Text scoreText;
+    public TextMeshProUGUI scoreText1;
+    public TextMeshProUGUI scoreText2;
 
     void Start() {
         int score = PlayerPrefs.GetInt("score");
-        scoreText.text = score.ToString();
+        scoreText1.text = score.ToString();
+        scoreText2.text = score.ToString();
     }
 
     public void BackGame ()
